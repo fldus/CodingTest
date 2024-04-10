8f2a4debb2fa6bf81e47a57edb98f71784111c4c
fldus <139347939+fldus@users.noreply.github.com>
2024-04-10 20:07:03 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        float sksnrl = ((float)num1 / num2)*1000;
+		int answer = (int)sksnrl;
+        
+        return answer;
+    }
+}
