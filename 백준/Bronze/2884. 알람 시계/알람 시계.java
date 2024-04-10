782e700a17bf70b2ed27939876ad29083ff7be9f
fldus <139347939+fldus@users.noreply.github.com>
2024-04-10 22:01:23 +0900

    [Bronze III] Title: 알람 시계, Time: 232 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+
+		if(m >= 45) {
+			System.out.println(h+" "+(m-45));
+		}else {
+			if(h == 0) h = 24;
+			System.out.println((h-1)+" "+(60-(45-m)));	// 60- (45 - m)
+		}
+		
+	}// main
+}
