7fbb5a2d0a248abdc5f6feb5018384598cb3c46b
fldus <139347939+fldus@users.noreply.github.com>
2024-04-12 23:46:41 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.15 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int number :array){
+            int count = map.getOrDefault(number,0) +1;
+                        
+            if(count > max){
+                max = count;
+                answer = number;
+            } 
+            else if(count == max) answer = -1;
+            
+            map.put(number, count);
+        }
+               
+        return answer;
+    }
+}
