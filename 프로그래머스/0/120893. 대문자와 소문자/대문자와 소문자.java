f6ad316535c8d696df7a6f392592ddf598032c9a
fldus <139347939+fldus@users.noreply.github.com>
2024-04-24 22:33:02 +0900

    [level 0] Title: 대문자와 소문자, Time: 11.36 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i=0; i<my_string.length(); i++){
+            char ch = my_string.charAt(i);
+            if(ch > 95)
+                answer += (char)(ch-32);
+            else
+                answer += (char)(ch+32);
+        }
+        
+        return answer;
+    }
+}
