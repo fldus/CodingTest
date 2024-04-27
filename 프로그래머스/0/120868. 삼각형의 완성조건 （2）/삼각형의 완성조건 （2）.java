3e343770e26b49cfa1f09331b14983bf74a48481
fldus <139347939+fldus@users.noreply.github.com>
2024-04-27 23:46:03 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        int max, min;
+        if(sides[0] > sides[1]){
+            max = sides[0];
+            min = sides[1];
+        }else{
+            max = sides[1];
+            min = sides[0];
+        }
+        
+        for(int i=1; i<=max; i++){
+            if(i+min > max)
+                answer++;
+        }
+        for(int i=max+1; i<max+min; i++)
+            answer++;
+        
+        return answer;
+    }
+}
