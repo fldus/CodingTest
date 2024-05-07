e2be4c713641d08d267fa256b34d12f55111ac38
fldus <139347939+fldus@users.noreply.github.com>
2024-05-07 22:29:56 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 187.96 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String answer = "";
+        
+        for(int i=0; i<a.length(); i++){
+            char ch = a.charAt(i);
+            if(Character.isLowerCase(ch))
+                answer += Character.toUpperCase(ch);
+            else
+                answer += Character.toLowerCase(ch);
+        }
+        
+        System.out.println(answer);
+        
+    }// main
+}
