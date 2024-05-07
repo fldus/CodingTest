3a5c62bae1d2a419396ceb28d0bc55bdd19a2928
fldus <139347939+fldus@users.noreply.github.com>
2024-05-07 22:22:43 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,23 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        char[] a = A.toCharArray();
-        
-        for(int i=0; i<A.length(); i++){
-        	if(B.equals(String.valueOf(a)))
-                break;
-            else {
-            	for(int j = a.length-1; j>0; j--){
-                    char temp = a[j];
-                    a[j] = a[j-1];
-                    a[j-1] = temp;
-            	}
-        		answer++;
-            }
-        }
-        if(answer == A.length())
-        	answer = -1;
-        
-        return answer;
+        B = B.repeat(2);
+        return B.indexOf(A);
     }
 }
