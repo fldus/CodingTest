70b3215a3045ffcc1e1b32f22097b0b602858b15
fldus <139347939+fldus@users.noreply.github.com>
2024-07-21 22:55:25 +0900

    [level 0] Title: OX퀴즈, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -2,13 +2,6 @@ def solution(quiz):
     answer = []
     for i in quiz:
         li = i.split(' ')
-        num = 0
-        if li[1] == '+':
-            num = int(li[0]) + int(li[2])
-        else:
-            num = int(li[0]) - int(li[2])
-        if num == int(li[4]):
-            answer.append('O')
-        else:
-            answer.append('X')
+        num = int(li[0])+int(li[2]) if li[1]=='+' else int(li[0])-int(li[2])
+        answer.append('O' if num==int(li[4]) else 'X')
     return answer
--- 프로그래머스/0/120907. OX퀴즈/README.md
+++ 프로그래머스/0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.04 ms
+메모리: 10.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 22:50:19
+2024년 07월 21일 22:55:23
 
 ### 문제 설명
 
